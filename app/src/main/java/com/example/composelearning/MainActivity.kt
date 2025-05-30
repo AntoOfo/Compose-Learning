2879ea07bd4cdb1996a101af2182db813f94d1a8
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-05-30 22:06:34 +0100

    padding stuff for greeting cards and the column as a whole

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -36,7 +37,8 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        Column(modifier) {
+        // this padding is for all the whole column in the apps background
+        Column(modifier = modifier.padding(vertical = 4.dp)) {
             for (name in names) {
                 Greeting(name = name)
             }
@@ -46,15 +48,21 @@ fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier = modifier.padding(24.dp)) { // padding inside column
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        // vertical and horizontal padding for space around each individual greeting card
+        // basically for each individual text and its background card
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Column(modifier = modifier
+            .padding(24.dp)
+            .fillMaxWidth()) {      // stretches row to fit width
             Text(text = "Hello")
             Text(text = "$name!")
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)  // widthDp to replicate a small phones width
 @Composable
 fun GreetingPreview() {
     ComposeLearningTheme {
