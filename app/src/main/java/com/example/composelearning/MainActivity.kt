b633fee33fa39f08fd84a7617dd605a84fdc73fc
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-01 23:19:55 +0100

    Second Step: alignyourbody element composable (circle image with text below)

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -5,9 +5,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.content.MediaType.Companion.Text
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -17,6 +25,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,6 +48,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// search bar composable
 @Composable
 fun SearchBar(modifier: Modifier = Modifier) {
     TextField(
@@ -57,6 +71,39 @@ fun SearchBar(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxWidth()  // fills width of screen
             .heightIn(min = 56.dp)  // height of search bar
+    )
+}
+
+// alignyourbody element composable (circle image with text below)
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier) {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,  // align columns contents to centre
+        modifier = modifier) {
+        Image(      // image
+            painter = painterResource(drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,    // crops image to fit in circle shape
+            modifier = Modifier
+                .size(88.dp)  // scale image down
+                .clip(CircleShape)  // shape
+        )
+        Text(text = stringResource(text),
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp))
+
+    }
+}
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyElementPreview() {
+    AlignYourBodyElement(
+        text = R.string.ab1_inversions,
+        drawable = R.drawable.ab1_inversion,
+        modifier = Modifier.padding(8.dp)
     )
 }
 
