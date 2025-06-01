41afb99a26caf80f9a1906dc001644c526ddefbc
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-01 21:47:18 +0100

    First step: search bar design

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -1,9 +1,25 @@
 package com.example.composelearning
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composelearning.ui.theme.ComposeLearningTheme
 
 
@@ -19,4 +35,34 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {     // for the small icon inside the searchbar
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+            focusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text("Search")   // hint inside search bar
+        },
+
+        modifier = modifier
+            .fillMaxWidth()  // fills width of screen
+            .heightIn(min = 56.dp)  // height of search bar
+    )
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    SearchBar()
+}
 
